80e1956b9a77383d4f0754e8a64908d3e45242d7
Zygis <zygisje@gmail.com>
2022-01-28 17:59:32 +0200

    Class Dictionary List ND

---
+++ classdictionarylistND/classdictionarylistND/DKlase.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdictionarylistND
+{
+    class DKlase
+    {
+        private Dictionary<string, string> zodynas;
+
+        public DKlase()
+        {
+            zodynas = new Dictionary<string, string>();
+            zodynas.Add("Zygimantas", "1");
+        }
+        public void PatikrintiArEgzistuoja()
+        {
+            if (zodynas.ContainsKey("Zygimantas"))
+            {
+                Console.WriteLine("Irasas egzistuoja zodyne");
+            }
+            else
+            {
+                Console.WriteLine("Irasas neegzistuoja zodyne");
+            }
+        }
+        public void PridetiIrasa()
+        {
+            zodynas.Add("Zygimante", "Jedenkute");
+            Console.WriteLine("Pridejome nauja irasa");
+        }
+        public void PaemimasIsZodyno()
+        {
+            Console.WriteLine($"{zodynas.TryGetValue("Zygimante", out string value)}");
+        }
+        public void GrazinimasVisuValues()
+        {
+            Dictionary<string, string>.ValueCollection values = zodynas.Values;
+            Console.WriteLine(zodynas.Values);
+        }
+    }
+}
---
+++ classdictionarylistND/classdictionarylistND/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classdictionarylistND
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DKlase klase = new DKlase();
+            
+            klase.PatikrintiArEgzistuoja();
+            klase.PridetiIrasa();
+            klase.PaemimasIsZodyno();
+            klase.GrazinimasVisuValues();
+
+            Console.ReadLine();
+            
+        }
+    }
+}
